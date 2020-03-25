c1c5ceb9f7f54e286b24bc98d05d93d618a1f9a9
Kwonyoungjoon <dudwns0911@naver.com>
2020-03-25 17:57:28 +0900

    home

---
+++ app/src/main/java/com/example/lifecycleseason2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lifecycleseason2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.word);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),Second_Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
